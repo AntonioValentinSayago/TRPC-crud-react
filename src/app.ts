7fec8123380d9b46d77a21380201b24f637cd2e4
AntonioValentinSayago <cesar_anto30@hotmail.com>
2023-02-07 20:49:29 -0600

    First Changes

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+import morgan from 'morgan'
+
+import * as trpc from '@trpc/server'
+import * as trpcExpress from '@trpc/server/adapters/express';
+import {router, createContext} from './trpc'
+
+const app = express()
+
+const appRouter = router({});
+
+app.use(morgan("dev"))
+
+app.use("trpc", trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext
+}))
+
+export default app
